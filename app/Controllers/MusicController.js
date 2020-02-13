19965e930f2bac462e761f825d2b908b49ca147b
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-02-13 11:03:28 -0700

    yaboi Bwiggs

--- app/Controllers/MusicController.js
+++ app/Controllers/MusicController.js
@@ -2,15 +2,19 @@ import MusicService from "../Services/MusicService.js";
 import store from "../store.js";
 
 //Private
-function _draw() {
+function _drawResults() {
   let songs = store.State.songs;
-  console.log(songs);
+  let template = "";
+  songs.forEach(s => {
+    template += s.previewTemplate;
+  });
+  document.getElementById("search-results").innerHTML = template;
 }
 
 //Public
 export default class MusicController {
   constructor() {
-    store.subscribe("songs", _draw);
+    store.subscribe("songs", _drawResults);
   }
   getMusicByQuery(event) {
     event.preventDefault();
@@ -18,5 +22,8 @@ export default class MusicController {
     MusicService.getMusicByQuery(formData.query.value);
 
     formData.reset();
+  }
+  setActive(id) {
+    MusicService.setActive(id); //not my problem
   }
 }
--- app/Models/Song.js
+++ app/Models/Song.js
@@ -5,6 +5,7 @@ export default class Song {
     this.title = data.title || data.trackName;
     this.preview = data.preview || data.previewUrl;
     this.price = data.price || data.trackPrice;
+    this.id = data._id || data.trackId;
     this.albumArt =
       data.albumArt || data.artworkUrl100.replace(/100x100/g, "300x300");
     this.albumArtSmall = this.albumArt.replace(/300x300/g, "90x90");
@@ -14,7 +15,7 @@ export default class Song {
   get previewTemplate() {
     return `
     <div class="col-12">
-    <div class="media">
+    <div class="media" onclick="app.musicController.setActive(${this.id})">
     <img src="${this.albumArtSmall}" class="mr-3" alt="...">
     <div class="media-body">
         <h5 class="mt-0">${this.artist} - ${this.title}</h5>
